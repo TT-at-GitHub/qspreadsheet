51bd88b6c6be45cbb4ee308b920e37d06954b805
tt <tt3d@start.bg>
2020-08-03 22:14:33 +0200

    testing generic delegates

--- dev.py
+++ dev.py
@@ -12,61 +12,28 @@ from fx import fx
 import PySide2
 plugin_path = os.path.join(os.path.dirname(PySide2.__file__), 'plugins', 'platforms')
 os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
-
 #In[0]
 area = pd.Series({0 : 423967, 1: 695662, 2: 141297, 3: 170312, 4: 149995})
-pop = pd.Series({0 : 38332521, 1: 26448193, 2: 19651127, 3: 19552860, 4: 12882135})
+population = pd.Series({0 : 38332521, 1: 26448193, 2: 19651127, 3: 19552860, 4: 12882135})
+population = population.astype(float)
 states = ['California', 'Texas', 'New York', 'Florida', 'Illinois']
-df = pd.DataFrame({'states':states, 'area':area, 'pop':pop}, index=range(len(states)))
-df.area = df.area.astype(float)
-df.iloc[0, 1] = 'c'
-df.iloc[3, 1] = 'a'
-s = df['area']
-s = fx.to_mixed_intstr(s)
-s.unique()
+df = pd.DataFrame({'states':states, 'area':area, 'population':population}, index=range(len(states)))
+dates = [pd.to_datetime('06-15-2020') + pd.DateOffset(i) for i in range(1, df.shape[0] + 1)]
+df['dates'] = dates
+df['bools'] = (df.index % 2 == 1)
+df['multip'] = df.population * 3.42 * df['bools']
+df['div'] = df.population / 2.3 * (~df['bools'])
+df['multip'] = (df['multip'] + df['div']).astype('float32')
+df['div'] = df['div'].astype(int)
+df.dtypes
 #In[0]
-fx.sort_mix_values(pd.Series(data=list(s.unique())))
+df.dtypes.map(str).str.contains('float')
 
 #In[0]
-set(s.astype(str))
-#In[0]
-
-import string
-import random
-
-rnd_txt = lambda: "".join( [random.choice(string.ascii_letters[:26]) for i in range(15)] )
-data = []
-for j in range(5):
-    r = []
-    for k in range(6):
-        r.append(rnd_txt())
-    r.append(random.randint(1,20))
-    r.append(random.random()*10)
-    data.append(r)
-df = pd.DataFrame(data, columns=['AAA','BBB','CCC','DDD','EEE','FFF','GGG','HHH'])
-
-#In[0]
-help(pd.Series.sort_values)
-
-#In[0]
-area = pd.Series({0 : 423967, 1: 695662, 2: 141297, 3: 170312, 4: 149995})
-pop = pd.Series({0 : 38332521, 1: 26448193, 2: 19651127, 3: 19552860, 4: 12882135})
-states = ['California', 'Texas', 'New York', 'Florida', 'Illinois']
-data = pd.DataFrame({'states':states, 'area':area, 'pop':pop}, index=range(len(states)))
-df = data
+df['dates'] = dates
 df
 #In[0]
-s = pd.Series(data=True, index=df.index)
-s.iloc[0] = False
-s
-
-# %%
-s.all()
-
-# %%
-st = QStyle()
-st
+df.population = df.population.astype('int32')
+df.dtypes
 #In[0]
-for attr in dir(QStyle):
-    if attr.startswith('SP_'):
-        print(attr)
+df.select_dtypes(include=['float32', 'float64'])
