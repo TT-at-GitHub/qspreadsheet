8bb05d31822129ea7c24abb0224fc3a51a58a420
tt <tt3d@start.bg>
2020-08-27 18:32:25 +0200

    adding column delegates

--- column_delegates.py
+++ column_delegates.py
@@ -1,29 +1,21 @@
-#!/usr/bin/env python3
-# Copyright (c) 2008-10 Qtrac Ltd. All rights reserved.
-# This program or module is free software: you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as published
-# by the Free Software Foundation, either version 2 of the License, or
-# version 3 of the License, or (at your option) any later version. It is
-# provided for educational purposes and is distributed in the hope that
-# it will be useful, but WITHOUT ANY WARRANTY; without even the implied
-# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
-# the GNU General Public License for more details.
-
+import os
 import platform
-import sys, os
+import sys
+
 import PySide2
-plugin_path = os.path.join(os.path.dirname(PySide2.__file__), 'plugins', 'platforms')
-os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
 from PySide2.QtCore import *
-from PySide2.QtWidgets import *
 from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+plugin_path = os.path.join(os.path.dirname(
+    PySide2.__file__), 'plugins', 'platforms')
+os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
 
 
 class RichTextLineEdit(QTextEdit):
 
     (Bold, Italic, Underline, StrikeOut, Monospaced, Sans, Serif,
      NoSuperOrSubscript, Subscript, Superscript) = range(10)
-
 
     def __init__(self, parent=None):
         super(RichTextLineEdit, self).__init__(parent)
@@ -37,40 +29,33 @@ class RichTextLineEdit(QTextEdit):
         self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         fm = QFontMetrics(self.font())
         h = int(fm.height() * (1.4 if platform.system() == "Windows"
-                                   else 1.2))
+                               else 1.2))
         self.setMinimumHeight(h)
         self.setMaximumHeight(int(h * 1.2))
         self.setToolTip("Press <b>Ctrl+M</b> for the text effects "
-                "menu and <b>Ctrl+K</b> for the color menu")
+                        "menu and <b>Ctrl+K</b> for the color menu")
 
-    
     def toggleItalic(self):
         self.setFontItalic(not self.fontItalic())
-
 
     def toggleUnderline(self):
         self.setFontUnderline(not self.fontUnderline())
 
-
     def toggleBold(self):
         self.setFontWeight(QFont.Normal
-                if self.fontWeight() > QFont.Normal else QFont.Bold)
-
+                           if self.fontWeight() > QFont.Normal else QFont.Bold)
 
     def sizeHint(self):
         return QSize(self.document().idealWidth() + 5,
                      self.maximumHeight())
 
-
     def minimumSizeHint(self):
         fm = QFontMetrics(self.font())
         return QSize(fm.width("WWWW"), self.minimumHeight())
 
-
     def contextMenuEvent(self, event):
         self.textEffectMenu()
 
-        
     def keyPressEvent(self, event):
         if event.modifiers() & Qt.ControlModifier:
             handled = False
@@ -98,7 +83,6 @@ class RichTextLineEdit(QTextEdit):
         else:
             QTextEdit.keyPressEvent(self, event)
 
-
     def colorMenu(self):
         pixmap = QPixmap(22, 22)
         menu = QMenu("Colour")
@@ -122,14 +106,12 @@ class RichTextLineEdit(QTextEdit):
         menu.exec_(self.viewport().mapToGlobal(
                    self.cursorRect().center()))
 
-
     def setColor(self):
         action = self.sender()
         if action is not None and isinstance(action, QAction):
             color = QColor(action.data())
             if color.isValid():
                 self.setTextColor(color)
-
 
     def textEffectMenu(self):
         format = self.currentCharFormat()
@@ -169,7 +151,6 @@ class RichTextLineEdit(QTextEdit):
         menu.exec_(self.viewport().mapToGlobal(
                    self.cursorRect().center()))
 
-
     def setTextEffect(self):
         action = self.sender()
         if action is not None and isinstance(action, QAction):
@@ -194,15 +175,14 @@ class RichTextLineEdit(QTextEdit):
                 format.setFontStrikeOut(not format.fontStrikeOut())
             if what == RichTextLineEdit.NoSuperOrSubscript:
                 format.setVerticalAlignment(
-                        QTextCharFormat.AlignNormal)
+                    QTextCharFormat.AlignNormal)
             elif what == RichTextLineEdit.Superscript:
                 format.setVerticalAlignment(
-                        QTextCharFormat.AlignSuperScript)
+                    QTextCharFormat.AlignSuperScript)
             elif what == RichTextLineEdit.Subscript:
                 format.setVerticalAlignment(
-                        QTextCharFormat.AlignSubScript)
+                    QTextCharFormat.AlignSubScript)
             self.mergeCurrentCharFormat(format)
-
 
     def toSimpleHtml(self):
         html = ""
@@ -218,7 +198,7 @@ class RichTextLineEdit(QTextEdit):
                     color = format.foreground().color()
                     text = QRegExp.escape(fragment.text())
                     if (format.verticalAlignment() ==
-                        QTextCharFormat.AlignSubScript):
+                            QTextCharFormat.AlignSubScript):
                         text = "<sub>{}</sub>".format(text)
                     elif (format.verticalAlignment() ==
                           QTextCharFormat.AlignSuperScript):
@@ -244,14 +224,155 @@ class RichTextLineEdit(QTextEdit):
         return html
 
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    lineedit = RichTextLineEdit()
-    lineedit.show()
-    lineedit.setWindowTitle("RichTextEdit")
-    app.exec_()
-    print(lineedit.toHtml())
-    print(lineedit.toPlainText())
-    print(lineedit.toSimpleHtml())
+class GenericDelegate(QStyledItemDelegate):
+
+    def __init__(self, parent=None):
+        super(GenericDelegate, self).__init__(parent)
+        self.delegates = {}
+
+    def insertColumnDelegate(self, column, delegate):
+        delegate.setParent(self)
+        self.delegates[column] = delegate
+
+    def removeColumnDelegate(self, column):
+        if column in self.delegates:
+            del self.delegates[column]
+
+    def paint(self, painter, option, index):
+        delegate = self.delegates.get(index.column())
+        if delegate is not None:
+            delegate.paint(painter, option, index)
+        else:
+            QStyledItemDelegate.paint(self, painter, option, index)
+
+    def createEditor(self, parent, option, index):
+        delegate = self.delegates.get(index.column())
+        if delegate is not None:
+            return delegate.createEditor(parent, option, index)
+        else:
+            return QStyledItemDelegate.createEditor(self, parent, option,
+                                                    index)
+
+    def setEditorData(self, editor, index):
+        delegate = self.delegates.get(index.column())
+        if delegate is not None:
+            delegate.setEditorData(editor, index)
+        else:
+            QStyledItemDelegate.setEditorData(self, editor, index)
+
+    def setModelData(self, editor, model, index):
+        delegate = self.delegates.get(index.column())
+        if delegate is not None:
+            delegate.setModelData(editor, model, index)
+        else:
+            QStyledItemDelegate.setModelData(self, editor, model, index)
 
 
+class IntegerColumnDelegate(QStyledItemDelegate):
+
+    def __init__(self, minimum=0, maximum=100, parent=None):
+        super(IntegerColumnDelegate, self).__init__(parent)
+        self.minimum = minimum
+        self.maximum = maximum
+
+    def createEditor(self, parent, option, index):
+        spinbox = QSpinBox(parent)
+        spinbox.setRange(self.minimum, self.maximum)
+        spinbox.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+        return spinbox
+
+    def setEditorData(self, editor, index):
+        value = int(index.model().data(index, Qt.DisplayRole))
+        editor.setValue(value)
+
+    def setModelData(self, editor, model, index):
+        editor.interpretText()
+        model.setData(index, editor.value())
+
+
+class DateColumnDelegate(QStyledItemDelegate):
+
+    def __init__(self, minimum=QDate(),
+                 maximum=QDate.currentDate(),
+                 format="yyyy-MM-dd", parent=None):
+        super(DateColumnDelegate, self).__init__(parent)
+        self.minimum = minimum
+        self.maximum = maximum
+        self.format = format
+
+    def createEditor(self, parent, option, index):
+        dateedit = QDateEdit(parent)
+        dateedit.setDateRange(self.minimum, self.maximum)
+        dateedit.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+        dateedit.setDisplayFormat(self.format)
+        dateedit.setCalendarPopup(True)
+        return dateedit
+
+    def setEditorData(self, editor, index):
+        value = index.model().data(index, Qt.DisplayRole)
+        editor.setDate(value)
+
+    def setModelData(self, editor, model, index):
+        model.setData(index, editor.date())
+
+
+class PlainTextColumnDelegate(QStyledItemDelegate):
+
+    def __init__(self, parent=None):
+        super(PlainTextColumnDelegate, self).__init__(parent)
+
+    def createEditor(self, parent, option, index):
+        lineedit = QLineEdit(parent)
+        return lineedit
+
+    def setEditorData(self, editor, index):
+        value = index.model().data(index, Qt.DisplayRole)
+        editor.setText(value)
+
+    def setModelData(self, editor, model, index):
+        model.setData(index, editor.text())
+
+
+class RichTextColumnDelegate(QStyledItemDelegate):
+
+    def __init__(self, parent=None):
+        super(RichTextColumnDelegate, self).__init__(parent)
+
+    def paint(self, painter, option, index):
+        text = index.model().data(index, Qt.DisplayRole)
+        palette = QApplication.palette()
+        document = QTextDocument()
+        document.setDefaultFont(option.font)
+        if option.state & QStyle.State_Selected:
+            document.setHtml("<font color={}>{}</font>".format(
+                palette.highlightedText().color().name(), text))
+        else:
+            document.setHtml(text)
+        painter.save()
+        color = (palette.highlight().color()
+                 if option.state & QStyle.State_Selected
+                 else QColor(index.model().data(index,
+                                                Qt.BackgroundColorRole)))
+        painter.fillRect(option.rect, color)
+        painter.translate(option.rect.x(), option.rect.y())
+        document.drawContents(painter)
+        painter.restore()
+
+    def sizeHint(self, option, index):
+        text = index.model().data(index)
+        document = QTextDocument()
+        document.setDefaultFont(option.font)
+        document.setHtml(text)
+        return QSize(document.idealWidth() + 5,
+                     option.fontMetrics.height())
+
+    def createEditor(self, parent, option, index):
+        lineedit = RichTextLineEdit(parent)
+        return lineedit
+
+    def setEditorData(self, editor, index):
+        value = index.model().data(index, Qt.DisplayRole)
+        editor.setHtml(value)
+
+    def setModelData(self, editor, model, index):
+        model.setData(index, editor.toSimpleHtml())
--- dev.py
+++ dev.py
@@ -4,43 +4,49 @@ from PySide2.QtCore import *
 from PySide2.QtWidgets import *
 from PySide2.QtGui import *
 import numpy as np
-import numpy
 import pandas as pd 
-
+from datetime import datetime, timedelta
 from fx import fx
+import qspreadsheet as qss
 
 import PySide2
 plugin_path = os.path.join(os.path.dirname(PySide2.__file__), 'plugins', 'platforms')
 os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
-#In[0]
-area = pd.Series({0 : 423967, 1: 695662, 2: 141297, 3: 170312, 4: 149995})
-population = pd.Series({0 : 38332521, 1: 26448193, 2: 19651127, 3: 19552860, 4: 12882135})
-population = population.astype(float)
-states = ['California', 'Texas', 'New York', 'Florida', 'Illinois']
-df = pd.DataFrame({'states':states, 
-    'area':area, 'population':population}, index=range(len(states)))
-dates = [pd.to_datetime('06-15-2020') + pd.DateOffset(i) for i in range(1, df.shape[0] + 1)]
-df['dates'] = dates
-df['bools'] = (df.index % 2 == 1)
-df['multip'] = df.population * 3.42 * df['bools']
-df['div'] = df.population / 2.3 * (~df['bools'])
-df['multip'] = (df['multip'] + df['div']).astype('float32')
-df['div'] = df['div'].astype('int32')
-df.dtypes
-#In[0]
-dftypes = df.dtypes.map(str)
-s = df.dtypes[ dftypes.str.contains('float')]
-s.index
-#In[0]
-dftypes.str.contains('int')
-dftypes.str.contains('bool')
-dftypes.str.contains('date')
+
+dt = datetime(2000, 7, 5)
+data = [
+    [5, 'Jim Carray',       dt + timedelta(days=1), True, 7.3],
+    [11, 'Kate Beckinsale', dt + timedelta(days=2), False, 1.69],
+    [9, 'Jim Carray',       dt + timedelta(days=4), True, 3.33],
+    [3, 'Jim Carray',       dt + timedelta(days=6), True, 14.12]
+    ]
+
+
+df = pd.DataFrame(data = data, columns=list('NSDBF'))
+df
+
+
 
 #In[0]
-df['dates'] = dates
-df
+dftypes = df.dtypes.map(str)
+dftypes
 #In[0]
-df.population = df.population.astype('int32')
+
+# print(df.columns[dftypes.str.contains('float')])
+# print(df.columns[dftypes.str.contains('int')])
+# print(df.columns[dftypes.str.contains('bool')])
+print(df.columns[dftypes.str.contains('date')])
+# print(df.columns[dftypes.str.contains('object')])
+df[df.columns[dftypes.str.contains('date')]]
+#In[0]
+
+df.select_dtypes(include=['float32', 'float64'])
 df.dtypes
 #In[0]
-df.select_dtypes(include=['float32', 'float64'])
+
+app = QApplication(sys.argv)
+
+df = mock_df()
+window = MainWindow(df)
+window.show()
+sys.exit(app.exec_())
