dc94f50560fb7ec502cc3471a2e9e891832f2692
tt <tt3d@start.bg>
2020-09-25 00:23:33 +0200

    copy to text

---
+++ cp2txt.py
@@ -0,0 +1,24 @@
+import os
+import sys 
+import shutil
+
+def copytree(src, dst, symlinks=False, ignore=None):
+
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+
+        if os.path.isdir(s):
+            shutil.copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
+
+dst = './.ignore/qspreadsheet.txt/'
+if os.path.isdir(dst):
+    shutil.rmtree(dst)
+    
+os.mkdir(dst)
+copytree('qspreadsheet/', dst)
+
+for fd in os.listdir(dst):
+    os.rename(os.path.join(dst, fd), os.path.join(dst, fd + '.txt'))
